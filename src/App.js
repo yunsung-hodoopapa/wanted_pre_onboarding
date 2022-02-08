5e0b513dcc71d22feef5d04eecb8d77e308f23c2
yunsung-hodoopapa <vappet3477@gamail.com>
2022-02-08 11:47:32 +0900

    feature-modal-component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import Modal from './component/Modal';
 
 const App = () => {
-  return <div className="App"></div>;
+  return <Modal />;
 };
 
 export default App;
